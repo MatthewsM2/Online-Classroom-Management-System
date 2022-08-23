cd1190fbefa2c1615226d606855f2a708498e4d6
Matthew M <matthew17773@gmail.com>
2022-08-23 18:02:43 +0530

    Second Commit in Front End

---
+++ Teacher_View/body.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teacher</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="styling.css">
+    <link rel="stylesheet" href="studentIcon.css">
+    <script src="/MiniProject/script.js"></script>
+</head>
+
+<body>
+    <div class="DivContainers">
+        <div class="subDiv">
+            <span class="material-symbols-outlined icons1" id="home" onclick="homeClick()" title="Home">
+                home_app_logo
+            </span>
+            <span class="material-symbols-outlined icons" title="Students">
+                groups
+            </span>
+            <span class="material-symbols-outlined icons" title="Create Modules">
+                edit_calendar
+            </span>
+            <span class="material-symbols-outlined icons" title="Attaendance">
+                new_releases
+            </span>
+            <span class="material-symbols-outlined icons" title="Assignment">
+                assignment
+            </span>
+            <span class="material-symbols-outlined icons" id="logOut" title="Sign Out">
+                logout
+            </span>
+        </div>
+
+        <div class="MainDiv MainDiv1">
+            <div class="titleContainer">
+                <h3>BCA-III</h3>
+                <h3>Class Code:6889hy4</h3>
+            </div>
+            <div class="outerContainer">
+                <h4>Module-1</h4>
+                <div class="subContainer">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="outerContainer">
+                <h4>Module-1</h4>
+                <div class="subContainer">
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71GrLKDeGRL.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/718KGMSXCAL.jpg" alt="">
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="MainDiv MainDiv2">
+            <div class="titleContainer">
+                <h3>BCA-III</h3>
+            </div>
+            <table id="StudentTable">
+                <tr>
+                    <th>Roll No</th>
+                    <th>Name</th>
+                    <th>Attendance %</th>
+                    <th>Assignments</th>
+                </tr>
+
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>John Mathew Thomas Jacob</td>
+                    <td>95%</td>
+                    <td>7/7</td>
+                </tr>
+
+                </tr>
+            </table>
+        </div>
+        <div class="MainDiv">
+            
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
--- Teacher_View/index_teacher.php
+++ Teacher_View/index_teacher.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="teacherCss.css">
+    <link rel="stylesheet" href="index_teacherCss.css">
     <script src="/MiniProject/script.js"></script>
 </head>
 
