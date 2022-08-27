5c13f2725590594911dd649a9827d1fd6ca32180
Matthew M <matthew17773@gmail.com>
2022-08-27 19:56:59 +0530

    Module Add Section Teacher front-end compled

--- Teacher_View/fileSelect.js
+++ Teacher_View/fileSelect.js
@@ -1,5 +1,7 @@
 const hideFilebtn = document.getElementById("selectFile");
 const chooseFile = document.getElementById("choose-file-click");
+const thumbnailInput=document.getElementById("Input-Type-File");
+const thumbnail=document.getElementById("thumb");
 chooseFile.addEventListener("click", function () {
   hideFilebtn.click();
 });
@@ -7,10 +9,20 @@ chooseFile.addEventListener("click", function () {
 
 hideFilebtn.addEventListener("change", function() {
   if (hideFilebtn.value) {
-    chooseFile.innerHTML = hideFilebtn.value.match(
+    chooseFile.value = hideFilebtn.value.match(
       /[\/\\]([\w\d\s\.\-\(\)]+)$/
     )[1];
   } else {
-    chooseFile.innerHTML = "No file chosen, yet.";
+    chooseFile.value = "";
   }
 });
+
+thumbnailInput.addEventListener("change",function(){
+  const [file] = thumbnailInput.files
+  if (file) {
+thumbnail.src=URL.createObjectURL(file);
+document.getElementsByClassName("uploadDiv-Icon")[0].style.display="none";
+document.getElementById("thumb").style.display="block";
+  }
+})
+
