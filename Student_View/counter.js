09e616f208c333c64e703306c115e5c1d850c765
Matthew M <matthew17773@gmail.com>
2022-09-05 16:04:53 +0530

    Java Script Added

--- Student_View/counter.js
+++ Student_View/counter.js
@@ -1,4 +1,21 @@
 let valueDisplays = document.querySelectorAll(".num");
+let interval = 2000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+function count(){
+  let valueDisplays = document.querySelectorAll(".num");
 let interval = 700;
 
 valueDisplays.forEach((valueDisplay) => {
@@ -13,3 +30,4 @@ valueDisplays.forEach((valueDisplay) => {
     }
   }, duration);
 });
+}
