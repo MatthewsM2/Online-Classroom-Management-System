5eb57c5b2e2bb4225dd4cab49724922963cd242a
Matthew M <matthew17773@gmail.com>
2022-09-07 23:01:28 +0530

    fixs needs in lowercase in placeholder and Login.php not working --login system failed to login

--- Login.php
+++ Login.php
@@ -1,3 +1,11 @@
 <?php
 $con = new mysqli("localhost","root","","smart_project") or die("Connection Failed");
+$usr=$_POST["loginUsrName"];
+$pass=$_POST["loginPassword"];
+echo $usr;
+echo $pass;
+$query = "SELECT `Account` FROM `Account` WHERE Usr_Name = '$usr';";
+var_dump($query);
+$acc=$con->query($query);
+echo $acc;
 ?>
