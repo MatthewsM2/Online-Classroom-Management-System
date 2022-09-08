b45143818bfa0499b4feeb6a54a34841311c2c5b
Matthew M <matthew17773@gmail.com>
2022-09-08 22:40:17 +0530

    Login.php login not ready

--- Login.php
+++ Login.php
@@ -4,22 +4,14 @@ $usr = $_POST["loginUsrName"];
 $pass = $_POST["loginPassword"];
 echo $usr;
 echo $pass;
-    $query = "SELECT * FROM Account WHERE Usr_Name= '$usr';";
-    $result = mysqli_query($connect, $query);
-    $rows = mysqli_num_rows($result);
-    var_dump($result);
-    if (!empty($rows)) {
-
-        // below codes will run properly without any error verified the system to change the login user and password to be checked and verified is needed
-        $queryUser = "SELECT Account FROM Account WHERE Usr_Name = '$usr';";
-        $acc = mysqli_query($con, $queryUser);
-        $resultAcc = mysqli_fetch_assoc($acc);
-        if ($resultAcc['Account'] == "teacher") {
-            echo "teacher";
-        } else if ($resultAcc['Account'] == "student") {
-            echo "student";
-        } else {
-            echo "page Not Found";
-        }
-    }
-
+// below codes will run properly without any error verified the system to change the login user and password to be checked and verified is needed
+$queryUser = "SELECT Account FROM Account WHERE Usr_Name = '$usr';";
+$acc = mysqli_query($con, $queryUser);
+$resultAcc = mysqli_fetch_assoc($acc);
+if ($resultAcc['Account'] == "teacher") {
+    echo "teacher";
+} else if ($resultAcc['Account'] == "student") {
+    echo "student";
+} else {
+    echo "page Not Found";
+}
