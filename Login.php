8bf1c839508e86bd46fe661e8beb8931aff75644
Matthew M <matthew17773@gmail.com>
2022-09-09 14:37:46 +0530

    Login system username and password error Not display in main page

--- Login.php
+++ Login.php
@@ -2,16 +2,14 @@
 $con = new mysqli("localhost", "root", "", "smart_project") or die("Connection Failed");
 $usr = $_POST["loginUsrName"];
 $pass = $_POST["loginPassword"];
-echo $usr;
-echo $pass;
-// below codes will run properly without any error verified the system to change the login user and password to be checked and verified is needed
-$queryUser = "SELECT Account FROM Account WHERE Usr_Name = '$usr';";
+// add some ajax code to check user name and password in live
+$queryUser = "SELECT Account FROM Account WHERE Usr_Name = '$usr' and pasword = '$pass';";
 $acc = mysqli_query($con, $queryUser);
 $resultAcc = mysqli_fetch_assoc($acc);
 if ($resultAcc['Account'] == "teacher") {
-    echo "teacher";
+    header("Location:http://localhost/MiniProject/Teacher_View/index_teacher.html");
 } else if ($resultAcc['Account'] == "student") {
-    echo "student";
+    header("Location:http://localhost/MiniProject/Student_View/student.html");
 } else {
     echo "page Not Found";
 }
