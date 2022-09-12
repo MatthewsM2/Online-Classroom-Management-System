deee7b04744be6115f1a3ecbb56217e50d7f6753
Matthew M <matthew17773@gmail.com>
2022-09-12 10:27:18 +0530

    Teacher Semester Class while loop Success

--- Teacher_View/index_teacher/index_tr_class_div.php
+++ Teacher_View/index_teacher/index_tr_class_div.php
@@ -4,31 +4,31 @@ $con = new mysqli("localhost", "root", "", "smart_project") or die("Connection F
 $queryGetTeacherValues = "SELECT * FROM `teacher_table` WHERE Teacher_UsrName = '$userName';";
 $GetTeacherValues = mysqli_query($con,$queryGetTeacherValues);
 $TeacherValuesRows = mysqli_num_rows($GetTeacherValues);
+
 if($TeacherValuesRows > 0){
-    echo '<div class="section">
-    <h3 id="semTitle">Semester-1</h5>
-        <!-- Main Div Container -->
-        <div class="DivClassContainer">
-            <!-- Subject Divs -->
-            <div class="SubjectClass">
-                <label for="SubjectName" class="SubClassTxt">BCA-1</label>
-                <span class="material-symbols-outlined delIcon">
-                    delete_forever
-                </span>
-                <label for="SubjectName" class="SubName">Python</label>
-            </div>
-            <div class="ArrowIconContainerRight" onclick="scrollArrowRight()">
-                <span class="material-symbols-outlined RightNextIcon">
-                    arrow_forward_ios
-                </span>
-            </div>
-        </div>
-</div>';  
+$queryUniqueSem="SELECT DISTINCT Semester FROM teacher_table;";
+$GetUniqueSem = mysqli_query($con,$queryUniqueSem);
+while($SemValtoTrav = mysqli_fetch_assoc($GetUniqueSem))
+{
+    $SemVal = $SemValtoTrav['Semester'];
+  echo'<div class="section">
+  <h3 id="semTitle">Semester-'.$SemVal.'</h5>
+  <div class="DivClassContainer">
+            <!-- Subject Divs -->';
+  $queryUniqueSemClass = "SELECT * FROM `teacher_table` WHERE Teacher_UsrName = '$userName' and Semester = '$SemVal';";
+  $GetUniqueSemClass = mysqli_query($con,$queryUniqueSemClass);
+  while($SemClassValtoTrav = mysqli_fetch_assoc($GetUniqueSemClass))
+  {
+   echo '<div class="SubjectClass">
+                <label for="SubjectName" class="SubClassTxt">'.$SemClassValtoTrav['Class_Name'].'</label>
+                
+                <label for="SubjectName" class="SubName">'.$SemClassValtoTrav['Subject'].'</label>
+            </div>';
+  }
+  echo'</div> </div>';
 }
-else{
-    echo '<div class="section">
-    
-</div> ';
 }
+
 ?>
+
  
