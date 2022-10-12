2733b3c1792132c66bd6bb0dd9fc370fe7463849
Matthew M <matthew17773@gmail.com>
2022-10-12 23:04:48 +0530

    smart logo add title bar

--- Teacher_View/index_teacher.php
+++ Teacher_View/index_teacher.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="http://localhost/MiniProject/Teacher_View/Assets/home_app_logo_FILL0_wght400_GRAD0_opsz48.svg" type="image/ico">
     <title>Teacher</title>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
